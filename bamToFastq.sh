24041f382260175574c5621c2dfd5e3556a99caf
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2017-11-15 23:46:43 +0800

    Update bamToFastq.sh

--- bamToFastq.sh
+++ bamToFastq.sh
@@ -10,7 +10,7 @@ echo "NCORE_REQ=${NCORE_REQ}"
 RT_LIMIT="48:00:00"
 MEM_LIMIT="125G" # it seems each job do not consume 10G to my observation; so set 125G shall be safe, even after increase the cores.
 
-FILE_COUNT=` find  /restricted/projectnb/chengcancer/Simon/project/Ho_lab/0raw/SN0126773/*.bam  | wc -l`
+FILE_COUNT=` find  /myPath/SN0126773/*.bam  | wc -l`
 echo "FILE_COUNT=${FILE_COUNT}"
 
 qsub -P $PROJECT -N $JOBNAME -pe omp $NCORE_REQ -l mem_total=${MEM_LIMIT} -l h_rt=${RT_LIMIT} -t 1-${FILE_COUNT}  bamToFastq.qsub
